e82dc5fa9262cefd0ffc6a32d9935f9e3d2395a3
dom2032784-lab <dom2032784@maricopa.edu>
2026-04-13 14:04:36 -0800

    Completed final review and updates before submission.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,14 +5,14 @@
 */
 
 // This statement sets the last modified date of the document in the footer
-document.getElementById("lastmodified").innerHTML = document.lastModified;
+document.getElementById("lastModified").innerHTML = document.lastModified;
 
-// This function changes the display an alert when the form is submitted
+// This function displays an alert when the form is submitted
 function formSubmit() {
     alert("Form submitted!");
 }   
 
 // This function shows the reset message when the form is reset
 function formReset() {
-    document.getElementById("resetmessage").style.visibility = "visible";
+    document.getElementById("resetMessage").style.visibility = "visible";
 } 
